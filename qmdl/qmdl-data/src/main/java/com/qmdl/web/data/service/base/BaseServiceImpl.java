8fca7de155bac50a29f3842aa1a91560a7e4fe6c
zhangxiaota <hyp_7@sina.cn>
2017-01-17 11:02:25 +0800

    Revert "1"
    This reverts commit 13d346eb09e86c5a1e3000a0a5fc3532e6c1fc82.

--- qmdl/qmdl-data/src/main/java/com/qmdl/web/data/service/base/BaseServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.qmdl.web.data.service.base;
-
-import org.mybatis.plus.mybatisplus.mapper.AutoMapper;
-import org.mybatis.plus.mybatisplus.service.impl.SuperServiceImpl;
-
-public class BaseServiceImpl<M extends AutoMapper<T>, T> extends SuperServiceImpl<M, T> {
-
-}
